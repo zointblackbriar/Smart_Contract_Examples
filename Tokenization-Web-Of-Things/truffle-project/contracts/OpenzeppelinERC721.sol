a842f53b39b6fe98f6dc2aae6797e85ceb785105
Orcun Oruc <orcun.oruc@tu-dresden.de>
2022-11-04 15:47:00 +0100

    [EDIT] Sample Connection for Java App aspect of Web of Things

--- Tokenization-Web-Of-Things/truffle-project/contracts/OpenzeppelinERC721.sol
+++ Tokenization-Web-Of-Things/truffle-project/contracts/OpenzeppelinERC721.sol
@@ -1,25 +1,31 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >= 0.8.7; 
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/drafts/Counters.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "./openzeppelincontracts/token/ERC721/ERC721.sol";
+import "./openzeppelincontracts/utils/Counters.sol";
+import "./openzeppelincontracts/access/Ownable.sol"; 
 
-contract OpenZeppelinERC721 {
+
+contract OpenZeppelinERC721 is ERC721, Ownable{
     // using Counters for Counters.Counter; 
     // Counters.Counter private _tokenIDs; 
-    mapping(address)
+    // mapping(address)
+    using Counters for Counters.Counter; 
+    event Logging(string description, uint value);
 
+    Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721Full("SampleItem", "ITM") public {
-
+    constructor() ERC721("SampleItem", "ITM") public {
     }
 
-    // function assignItem(address player, string memory tokenURI) public returns(uint256) {
-    //     _tokenIDs.increment(); 
+    function safeMint(address _toAddress) public onlyOwner{
+        uint tokenId = _tokenIdCounter.current();
+        emit Logging("token id is: ", tokenId);
+        _tokenIdCounter.increment(); 
+        _safeMint(_toAddress, tokenId);
+    }
 
-    //     uint newItemID = _tokenIDs.current(); 
-    //     _mint(player, newItemId); 
-    //     _setTokenURI(newItemId, tokenURI); 
-
-    // }
 }
