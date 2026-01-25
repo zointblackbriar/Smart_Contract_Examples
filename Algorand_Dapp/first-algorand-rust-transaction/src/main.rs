9dc2b820da248b4ba0b9db56d694c7e0a7b82dae
zoint <mountidablog@gmail.com>
2026-01-25 11:12:22 +0100

    deleted files were inserted

--- Algorand_Dapp/first-algorand-rust-transaction/src/main.rs
+++ Algorand_Dapp/first-algorand-rust-transaction/src/main.rs
@@ -1,26 +1,38 @@
-use algonaut::core::{Address, MicroAlgos};
-use algonaut::transaction::{BaseTransaction, Payment, Transaction, TransactionType};
-use algonaut::{Algod, Kmd};
+use algonaut::Kmd;
 use std::error::Error;
-use std::env;
-// use dotenv::dotenv;
-
-
 
 fn main() -> Result<(), Box<dyn Error>> {
-
-    // dotenv().ok();
-    // println!("Hello, world!");
     let kmd = Kmd::new()
         .bind("http://localhost:4002")
         .auth("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
         .client_v1()?;
 
-    println!("result of the kmd is: {}", kmd ); 
-
-    let list_response = kmd.list_wallets();
-    let wallet_id = match list_response
-
+    let list_response = kmd.list_wallets()?;
+    if let Some(wallet) = list_response.wallets.get(0) {
+        println!("Wallet Name: {}, ID: {}", wallet.name, wallet.id);
+    } else {
+        println!("No wallets found.");
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // This is an integration test that requires a running kmd instance.
+    #[test]
+    fn list_wallets_integration_test() -> Result<(), Box<dyn Error>> {
+        let kmd = Kmd::new()
+            .bind("http://localhost:4002")
+            .auth("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+            .client_v1()?;
+
+        let list_response = kmd.list_wallets();
+        
+        assert!(list_response.is_ok());
+
+        Ok(())
+    }
 }
